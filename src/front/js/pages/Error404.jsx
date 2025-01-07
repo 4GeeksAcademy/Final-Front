e6419ba9acf7b9abdccea1340ee341f1b078b1dd
Alejandro Quiles García <alexqg1208@gmail.com>
2025-01-07 18:18:07 +0000

    feat error 404

---
+++ src/front/js/pages/Error404.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Error404 = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">Error 404</h1>
+        </div>
+    )
+}
